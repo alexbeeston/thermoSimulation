97ff64ec0c8b7d1d159eb89dff3c865ebe6e4eac
= <alexmbeeston@gmaill.com>
2019-09-26 17:02:58 -0600

    first final draft

--- sim.py
+++ sim.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 ########################
 ### HELPER FUNCTIONS ###
 ########################
+def getdataFileName():
+    logFileName = "data.csv"
+    if len(sys.argv) > 2:
+        print("Usage Error: sim.py [outPutFileName]")
+        sys.exit()
+    elif len(sys.argv) == 2:
+        logFileName = sys.argv[1]
+    return logFileName
+    
 def calcWeightedAverage(mass1, temp1, mass2, temp2):
     return (mass1 * temp1 + mass2 * temp2) / (mass1 + mass2)
 
@@ -44,20 +53,65 @@ def graphResults(times, hotTemperatures, coldTemperatures):
         print(
             "The graph of the data could not be generated. Try running the command \"pip install matplotlib\" to install the necessary modules")
 
-# Open Data Output File
 def openDataFile():
-    logFileName = "data.csv"
-    if len(sys.argv) > 2:
-        print("Usage Error: sim.py [outPutFileName]")
-        sys.exit()
-    elif len(sys.argv) == 2:
-        logFileName = sys.argv[1]
-    dataFile = open(logFileName, "w")
+    dataFile = open(getdataFileName(), "w")
     headers = "Iteration,Time Stamp (sec),Hot Temperature (Cel),Cold Temperature (Cel)\n"
     dataFile.write(headers)
     dataFile.write("0,0," + str(hotTemp) + "," + str(coldTemp) + "\n")
     return dataFile
 
+def validate(stepSize, massFlow, hotTemp, coldTemp, heaterVolume, tankVolume, duration, maxTemp, minTemp, p):
+    for pair in p.items():
+        if float(pair[1]) < 0:
+            print("Parameter Error: \"" + pair[0] + "\" must be greater than zero.")
+            sys.exit()
+    if stepSize * massFlow > densityWater * heaterVolume:
+        print("Parameter error: The step size is too large for the size of the heater.\nThe following comparison must hold: (stepSize * massFlowRate) < (densityWater * heaterElementVolume).")
+        print("Do at least one of the following:\n1) Decrease step size\n2) decrease mass flow rate\n3) increase volume of heating element")
+        sys.exit()
+    if stepSize * massFlow > densityWater * tankVolume:
+        print("Parameter error: The step size is too large for the size of the tank.\nThe following comparison must hold: (stepSize * massFlowRate) < (densityWater * storageTankVolume).")
+        print("Do at least one of the following:\n1) Decrease step size\n2) decrease mass flow rate\n3) increase volume of storage tank")
+        sys.exit()
+    if stepSize > duration:
+        print("Parameter error: The step size is larger than the run time. The step size must be smaller than the runn time of the simulation")
+        sys.exit()
+    if hotTemp > 100:
+        print("Parameter error: The initial hot temp is larger than 100 degrees celsius. Enter a value in the range (0, 100)")
+        sys.exit()
+    if coldTemp > 100:
+        print("Parameter error: The initial cold temp is larger than 100 degrees celsius. Enter a value in the range (0, 100)")
+        sys.exit()
+    if maxTemp > 100:
+        print("Parameter error: The maximum permissible temperature is greater than 100 degrees celsius. This simulation only support liquid fluids.")
+        sys.exit()
+    if maxTemp < minTemp:
+        print("Parameter error: The maximum permissible temperature is less than the minimum permissible temperature")
+        sys.exit()
+
+def printResults(iterations, hotTemp_previous, hotConvergeIteration, coldTemp_previous, coldConvergeIteration, stepSize, p):
+    print("System Configurations:")
+    print("   Simulated run time: " + str(duration / 60) + " min")
+    print("   Step Size: " + p["step size (sec)"] + " sec")
+    print("   Heat input: " + p["heat in (kWatts)"] + " kW")
+    print("   Heat output: " + p["heat out (kWatts)"] + " kW")
+    print("   Mass flow rate: " + p["mass flow rate (kg/sec)"] + " kg/sec")
+    print("   Initial hot temperature: " + p["initial hot temp (cel)"] + " deg C")
+    print("   Initial cold temperature: " + p["initial cold temp (cel)"] + " deg C")
+    print("   Volume of heating element: " + p["heating element volume (m^3)"] + " m^3")
+    print("   Volume of storage tank: " + p["storage tank volume (m^3)"] + " m^3")
+    print("   Convergence criteria used: " + p["converge criteria (unitless)"])
+    print("   Maximum permissible temperature: " + p["maximum permissible temperature (cel)"] + " deg C")
+    print("   Minimum permissible termperature: " + p["minimum permissible temperature (cel)"] + " deg C")
+    if hotConvergeIteration < iterations:
+        print("The warm region converged to a temperature of " + str(hotTemp_previous) + " on iteration " + str(hotConvergeIteration) + ", which maps to " + str(hotConvergeIteration * stepSize / 60) + " minutes of simulation time.")
+    else:
+        print("The warm region did not converge. The final temperature was " + str(hotTemp_previous) + " degrees celsius.")
+    if coldConvergeIteration < iterations:
+        print("The cool region converged to a temperature of " + str(coldTemp_previous) + " on iteration " + str(coldConvergeIteration) + ", which maps to " + str(coldConvergeIteration * stepSize / 60) + " minutes of simulation time.")
+    else:
+        print("The cool region did not converge. The final temperature was " + str(coldTemp_previous) + " degrees celsius.")
+    print("See the file " + getdataFileName() + " for a complete report of the temperatures.")
 
 ###################
 ### ENTRY POINT ###
@@ -84,44 +138,9 @@ heaterVolume = float(p["heating element volume (m^3)"])
 tankVolume = float(p["storage tank volume (m^3)"])
 duration = float(p["run time (min)"]) * 60
 convergenceCriteria = float(p["converge criteria (unitless)"])
-maxTemp = float(p["maximum permissible temperature"])
-minTemp = float(p["minimum permissible temperature"])
-stopOnConverge_int = int(p["stop on convergence (0=false 1=true)"])
-stopOnConverge = False
-if stopOnConverge_int == 1:
-    stopOnConverge = True
-
-# validate parameters
-for pair in p.items():
-    if float(pair[1]) < 0:
-        print("Parameter Error: \"" + pair[0] + "\" must be greater than zero.")
-        sys.exit()
-if not (stopOnConverge_int == 0 or stopOnConverge_int == 1):
-    print("Parameter error: \"stop on convergence (0=false 1=true)\" must be set to either 0 or 1")
-    sys.exit()
-if stepSize * massFlow > densityWater * heaterVolume:
-    print("Parameter error: The step size is too large for the size of the heater.\nThe following comparison must hold: (stepSize * massFlowRate) < (densityWater * heaterElementVolume).")
-    print("Do at least one of the following:\n1) Decrease step size\n2) decrease mass flow rate\n3) increase volume of heating element")
-    sys.exit()
-if stepSize * massFlow > densityWater * tankVolume:
-    print("Parameter error: The step size is too large for the size of the tank.\nThe following comparison must hold: (stepSize * massFlowRate) < (densityWater * storageTankVolume).")
-    print("Do at least one of the following:\n1) Decrease step size\n2) decrease mass flow rate\n3) increase volume of storage tank")
-    sys.exit()
-if stepSize > duration:
-    print("Parameter error: The step size is larger than the run time. The step size must be smaller than the runn time of the simulation")
-    sys.exit()
-if hotTemp > 100:
-    print("Parameter error: The initial hot temp is larger than 100 degrees celsius. Enter a value in the range (0, 100)")
-    sys.exit()
-if coldTemp > 100:
-    print("Parameter error: The initial cold temp is larger than 100 degrees celsius. Enter a value in the range (0, 100)")
-    sys.exit()
-if maxTemp > 100:
-    print("Parameter error: The maximum permissible temperature is greater than 100 degrees celsius. This simulation only support liquid fluids.")
-    sys.exit()
-if maxTemp < minTemp:
-    print("Parameter error: The maximum permissible temperature is less than the minimum permissible temperature")
-    sys.exit()
+maxTemp = float(p["maximum permissible temperature (cel)"])
+minTemp = float(p["minimum permissible temperature (cel)"])
+validate(stepSize, massFlow, hotTemp, coldTemp, heaterVolume, tankVolume, duration, maxTemp, minTemp, p)
 
 # generate run-time parameters
 fidelity = stepSize * massFlow # kg
@@ -129,13 +148,13 @@ iterations = duration * massFlow / fidelity # unitless
 heaterMass = heaterVolume * densityWater # kg
 tankMass = tankVolume * densityWater #kg
 
-# define variables vsed for determining convergence
+# define variables used for determining convergence
 hotTemp_previous  = hotTemp
 coldTemp_previous = hotTemp
-hotTemp_converge = -1
-coldTemp_converge = -1
-hotTemp_converge_flag = False
-coldTemp_converge_flag = False
+hotConvergeIteration = -1
+coldConvergeIteration = -1
+hotFlag = False
+coldFlag = False
 
 # initialize simulation data, flow control variables, and open log file
 times = [0]
@@ -174,38 +193,19 @@ while i < int(iterations) and keepGoing:
     coldTemperatures.append(coldTemp)
 
     # check for convergence
-    if abs(hotTemp_previous - hotTemp) < convergenceCriteria and not hotTemp_converge_flag:
-        hotTemp_converge = i
-        hotTemp_converge_flag = True
-    else:
+    if abs(hotTemp_previous - hotTemp) < convergenceCriteria and not hotFlag:
+        hotFlag = True
+        hotConvergeIteration = i
+    elif not hotFlag:
         hotTemp_previous = hotTemp
-    if abs(coldTemp_previous - coldTemp) < convergenceCriteria and not coldTemp_converge_flag:
-        coldTemp_converge = i
-        coldTemp_converge_flag = True
-    else:
+    if abs(coldTemp_previous - coldTemp) < convergenceCriteria and not coldFlag:
+        coldFlag = True
+        coldConvergeIteration = i
+    elif not coldFlag:
         coldTemp_previous = coldTemp
     i += 1
 
-
+# log and visualize data
 dataFile.close()
+printResults(iterations, hotTemp_previous, hotConvergeIteration, coldTemp_previous, coldConvergeIteration, stepSize, p)
 graphResults(times, hotTemperatures, coldTemperatures)
-
-# log the results of the computation
-log = open("log.txt", "w")
-log.write("Initial hot temperature: " + p["initial hot temp (cel)"] + "\n")
-log.write("Initial cold temperature: " + p["initial cold temp (cel)"] + "\n")
-
-
-print("For step size = " + str(stepSize) + " (used " + str(iterations) + " iterations).")
-print("Final hotTemp: " + str(hotTemp))
-print("Final coldTemp: " + str(coldTemp))
-if hotTemp_converge_flag:
-    print("hotTemp converged after " + str(hotTemp_converge + 1) + " iterations, which maps to " + str(hotTemp_converge * stepSize) + " seconds of run time.")
-else:
-    print("hotTemp did not converge. Final difference: " + str(abs(hotTemp_previous - hotTemp)))
-
-if coldTemp_converge_flag:
-    print("coldTemp converged after " + str(coldTemp_converge + 1) + " iterations, which maps to " + str(coldTemp_converge * stepSize) + " seconds of run time.")
-else:
-    print("coldTemp did not converge. Final difference: " + str(abs(coldTemp_previous - coldTemp)))
-
