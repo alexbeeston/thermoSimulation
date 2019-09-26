a883d37bbba201e29e7b72e3ee3700c60e52270c
Alex Beeston <A01900968@aggies.usu.edu>
2019-09-26 12:30:26 -0600

    functionality for config file

--- sim.py
+++ sim.py
@@ -1,77 +1,101 @@
 import sys
 
-stepSize = 1 # sec
-heatRate = 1000 # kWatts - separate into two
-massFlow = 5 # kg / sec
-temp1 = 20 # Celsius (initial temperature of hot water)
-temp2 = 12 # Celsius (initial temperature of cold water)
-massSolar = 25 # kg
-massTank = 750 # kg
-specificHeat = 4.184 # kJ/(kg * C)
-duration = 5000 # sec
-convergeCriteria = .001 # unit less
+
 
 # for numerical methods stuff
-temp1_previous  = 10
-temp2_previous = 10
-temp1_converge = -1
-temp2_converge = -1
-temp1_converge_flag = False
-temp2_converge_flag = False
+hotTemp_previous  = 10
+coldTemp_previous = 10
+hotTemp_converge = -1
+coldTemp_converge = -1
+hotTemp_converge_flag = False
+coldTemp_converge_flag = False
 
-# BEGIN
+
+
+
+# Define Thermodynamic Constants
+specificHeatWater = 4.184 # kJ/(kg * C)
+densityWater = 1000 # kg/m^3
+
+# Parse Parameters File
+parameterFile = open("parameters.txt", "r")
+p = {}
+for line in parameterFile:
+    words = line.strip().split(",")
+    p[words[0]] = words[1]
+parameterFile.close()
+
+stepSize = float(p["step size"]) # sec
+heatIn = float(p["heat in"]) # kWatts
+heatOut = float(p["heat out"]) #kWatts
+massFlow = float(p["mass flow rate"]) # kg / sec
+hotTemp = float(p["initial hot temp"]) # Celsius (initial temperature of hot water)
+coldTemp = float(p["initial cold temp"]) # Celsius (initial temperature of cold water)
+heaterVolume = float(p["heating element volume"])# m^3
+tankVolume = float(p["storage tank volume"]) # m^3
+duration = float(p["run time"]) # sec
+convergenceCriteria = float(p["converge criteria"]) # unit less
+
+# Generate Run-Time Parameters
 fidelity = stepSize * massFlow # kg
-iterations = duration * massFlow / fidelity # unit less
-log = open("log.txt", "w")
+iterations = duration * massFlow / fidelity # unitless
+heaterMass = heaterVolume * densityWater # kg
+tankMass = tankVolume * densityWater #kg
+
+# Open Data Output File
+logFileName = "data.csv"
+if len(sys.argv) > 2:
+    print("Usage Error: sim.py [outPutFileName]")
+    sys.exit()
+elif len(sys.argv) == 2:
+    logFileName = sys.argv[1]
+log = open(logFileName, "w")
 headers = "iteration,timeStamp,Q_in,temp_1,Q_out,temp_2"
+log.write(headers)
+
+# Run Simulation
 for i in range(int(iterations)):
     # print("Iteration " + str(i) + ":")
     # step 1: mix water from pipe with water in heater
-    temp1 = (fidelity * temp2 + (massSolar - fidelity) * temp1) / massSolar # a weighted average, see appendix 1 for derivation, equation 1, whatever
+    hotTemp = (fidelity * coldTemp + (heaterMass - fidelity) * hotTemp) / heaterMass # a weighted average, see appendix 1 for derivation, equation 1, whatever
 
     # step 2: add heat to ALL  water in heater
-    addedHeat = heatRate * stepSize
-    temp1 = temp1 + addedHeat / (massSolar * specificHeat)
-    #print("   temp1: " + str(temp1))
+    addedHeat = heatIn * stepSize
+    hotTemp = hotTemp + addedHeat / (heaterMass * specificHeatWater)
+    #print("   hotTemp: " + str(hotTemp))
 
     # step 3: add warm water to tank
-    temp2 = (fidelity * temp1 + (massTank - fidelity) * temp2) / massTank
+    coldTemp = (fidelity * hotTemp + (tankMass - fidelity) * coldTemp) / tankMass
 
     # step 4: remove heat from ALL water in tank
-    addedHeat = -heatRate * stepSize
-    temp2 = temp2 + addedHeat / (massTank * specificHeat)
+    addedHeat = heatOut * stepSize
+    coldTemp = coldTemp + addedHeat / (tankMass * specificHeatWater)
 
     # report
 
 
-    if abs(temp1_previous - temp1) < convergeCriteria and not temp1_converge_flag:
-        temp1_converge = i
-        temp1_converge_flag = True
+    if abs(hotTemp_previous - hotTemp) < convergenceCriteria and not hotTemp_converge_flag:
+        hotTemp_converge = i
+        hotTemp_converge_flag = True
     else:
-        temp1_previous = temp1
+        hotTemp_previous = hotTemp
 
-    if abs(temp2_previous - temp2) < convergeCriteria and not temp2_converge_flag:
-        temp2_converge = i
-        temp2_converge_flag = True
+    if abs(coldTemp_previous - coldTemp) < convergenceCriteria and not coldTemp_converge_flag:
+        coldTemp_converge = i
+        coldTemp_converge_flag = True
     else:
-        temp2_previous = temp2
+        coldTemp_previous = coldTemp
 
-print("For step size = " + str(stepSize) + " (used " + str(iterations) + " iterations).")
-print("Final temp1: " + str(temp1))
-print("Final temp2: " + str(temp2))
-if temp1_converge_flag:
-    print("temp1 converged after " + str(temp1_converge + 1) + " iterations, which maps to " + str(temp1_converge * stepSize) + " seconds of run time.")
-else:
-    print("temp1 did not converge. Final difference: " + str(abs(temp1_previous - temp1)))
-
-if temp2_converge_flag:
-    print("temp2 converged after " + str(temp2_converge + 1) + " iterations, which maps to " + str(temp2_converge * stepSize) + " seconds of run time.")
-else:
-    print("temp2 did not converge. Final difference: " + str(abs(temp2_previous - temp2)))
 log.close()
+print("For step size = " + str(stepSize) + " (used " + str(iterations) + " iterations).")
+print("Final hotTemp: " + str(hotTemp))
+print("Final coldTemp: " + str(coldTemp))
+if hotTemp_converge_flag:
+    print("hotTemp converged after " + str(hotTemp_converge + 1) + " iterations, which maps to " + str(hotTemp_converge * stepSize) + " seconds of run time.")
+else:
+    print("hotTemp did not converge. Final difference: " + str(abs(hotTemp_previous - hotTemp)))
 
-'''
-BUG:
-- monitor the fidelity does not divide the mass flow rate
-
-'''
+if coldTemp_converge_flag:
+    print("coldTemp converged after " + str(coldTemp_converge + 1) + " iterations, which maps to " + str(coldTemp_converge * stepSize) + " seconds of run time.")
+else:
+    print("coldTemp did not converge. Final difference: " + str(abs(coldTemp_previous - coldTemp)))
